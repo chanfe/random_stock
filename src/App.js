66350626f18cf99d0ebfcfd3ccf726412a7efd72
Felix Chan <felixchan29@gmail.com>
2020-09-02 23:08:20 -0400

    grab all of symbols

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import symbols from './assests/sym.env'
 
 function App() {
+
+  console.log(symbols)
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/assests/parsefile.py
@@ -0,0 +1,18 @@
+file_read = open('nasdaqlisted.txt', 'r')
+file_write = open('sym.env', 'w')
+
+Lines = file_read.readlines()
+
+sym = []
+
+for num in range(len(Lines)):
+    if num == 0 or num == len(Lines)-1:
+        continue
+    sym.append(Lines[num].split('|')[0])
+
+string = "symbols=[" + ', '.join(sym) + "]"
+
+file_write.write(string)
+file_read.close
+file_write.close
+
