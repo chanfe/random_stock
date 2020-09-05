b409375c48d156214556d65d62af832b826f6923
Felix Chan <felixchan29@gmail.com>
2020-09-05 11:00:18 -0400

    added links to dirct to yahoo or robinhood of that symbol

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
       <header className="App-header">
         <h1>{random_symbol}</h1>
         <button onClick={button_pressed}>Generate random Nasdaq Symbol</button>
+        <br />
+        <a href={"https://robinhood.com/stocks/" + random_symbol}>Robinhood</a>
+        <a href={"https://finance.yahoo.com/quote/" + random_symbol}>Yahoo</a>
       </header>
       
     </div>
