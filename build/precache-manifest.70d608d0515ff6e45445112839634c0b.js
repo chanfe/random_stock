fc39774ea5d51742f070c2caf027c6e01e83b971
Felix Chan <felixchan29@gmail.com>
2020-09-16 21:29:56 -0400

    build

---
+++ build/precache-manifest.70d608d0515ff6e45445112839634c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407c02ddb88029a406804b6d930f1eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e92a081d5a32e31a7a",
+    "url": "/static/css/main.2bd7ec21.chunk.css"
+  },
+  {
+    "revision": "28eae8640628ed70faba",
+    "url": "/static/js/2.0f536de1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f536de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e92a081d5a32e31a7a",
+    "url": "/static/js/main.1ec94223.chunk.js"
+  },
+  {
+    "revision": "a0e866e3ac72e5a24624",
+    "url": "/static/js/runtime-main.b2170f77.js"
+  }
+]);
